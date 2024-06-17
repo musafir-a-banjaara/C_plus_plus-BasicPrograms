2473cd93bdf326288d016cdc3a50eaab4c67a751
KUNAL RANJAN <kunal.work786@gmail.com>
2024-06-17 11:48:19 +0530

    New Repo inside an existing repo

---
+++ ConditionsAndLoops/EvenOrOdd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number that you want to check for Even or Odd : ";
+    cin>>n;
+    if(n%2==0)
+    cout<<"The Number entered is Even "<<endl;
+    else
+    cout<<"The Number entered is Odd "<<endl;
+    return 0;
+}
---
+++ ConditionsAndLoops/README.md
@@ -0,0 +1,2 @@
+1. EvenOrOdd.cpp checks the number entered by user to be Even or Odd
+<br>
